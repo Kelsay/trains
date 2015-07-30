97ca94618469e659dff61fd6b54e5d664dfd512f
U-Undercity\Kelsay <krzysztof@the-earthworks.com>
2015-07-30 13:27:49 +0100

    [FE] Added support for HTML5 urls (redirect)

---
+++ TrainingFrontEnd/Code/MainApplication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Training.Code
+{
+    public class MainApplication : HttpApplication
+    {
+
+        private const string ROOT_DOCUMENT = "/index.html";
+
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            string url = Request.Url.LocalPath;
+            if (!System.IO.File.Exists(Context.Server.MapPath(url)))
+                Context.RewritePath(ROOT_DOCUMENT);
+        }
+
+    }
+
+}
